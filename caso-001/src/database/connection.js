f4222a9188ca6f6d1daf6c77c4101e0eaaa17670
apuu03 <57383340+apuu03@users.noreply.github.com>
2025-06-13 17:49:18 -0300

    Fix: agregado UTF-8 charset para que se reconozcan caracteres especiales al momento de construir la db

--- caso-001/src/database/connection.js
+++ caso-001/src/database/connection.js
@@ -8,6 +8,9 @@ const pool = mysql.createPool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  charset: 'utf8mb4'
 });
+
+
 
 module.exports = { pool };
